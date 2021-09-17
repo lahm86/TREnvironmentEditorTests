b864608b38d09aaefd8ed6f1e62bf0a988343bd9
lahm86 <33758420+lahm86@users.noreply.github.com>
2021-09-17 18:56:30 +0100

    Improvements to control
    - Allows finer control via the command line, e.g. looping, specifying Lara's position etc.
    - Update to location checks.

--- TREnvironmentEditorTests/Program.cs
+++ TREnvironmentEditorTests/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using TREnvironmentEditor;
 using TREnvironmentEditor.Helpers;
 using TREnvironmentEditor.Model;
 using TREnvironmentEditor.Model.Types;
+using TRFDControl;
+using TRFDControl.FDEntryTypes;
+using TRFDControl.Utilities;
 using TRLevelReader;
 using TRLevelReader.Helpers;
 using TRLevelReader.Model;
@@ -14,151 +17,251 @@ namespace TREnvironmentEditorTests
 {
     class Program
     {
-        static void Main(string[] args)
+        static TR2LevelReader _reader = new TR2LevelReader();
+        static TR2LevelWriter _writer = new TR2LevelWriter();
+
+        static string _readPath, _writePath, _laraPos, _laraItems;
+        static EMType[] _exclusions = new EMType[] { };
+
+        static void Main()
         {
             string rdir = @"D:\Games\steamapps\common\Tomb Raider (II) - Untouched\data\";
             string wdir = @"D:\Games\steamapps\common\Tomb Raider (II) - UKBox Dev\data\";
 
-            string lvl = LevelNames.LQ;
-            TR2LevelReader reader = new TR2LevelReader();
-            TR2LevelWriter writer = new TR2LevelWriter();
+            _laraPos = "34776, 10496, 42330, 17";
+            _laraItems = "112";
 
-            EMEditorMapping mapping = EMEditorMapping.Get(lvl);
-            EMType[] exclusions = new EMType[] { };
+            List<string> lvls = LevelNames.AsListWithAssault;
 
-            Console.WriteLine("0 - All");
-            Console.WriteLine("1 - Any");
-            Console.WriteLine("2 - AllWithin");
-            Console.WriteLine("3 - OneOf");
-            Console.WriteLine("4 - Validate locations");
-
-            int opt = GetInt("Pick an option:");
-
-            if (mapping.All.Count > 0 && opt == 0)
+            while (true)
             {
-                TR2Level level = reader.ReadLevel(rdir + lvl);
-                mapping.All.ApplyToLevel(level, exclusions);
-                MoveLara(level);
-                writer.WriteLevelToFile(level, wdir + lvl);
-                Console.WriteLine("All AL mods applied, level saved.");
-            }
-            else if (mapping.Any.Count > 0 && opt == 1)
-            {
-                TR2Level level = reader.ReadLevel(rdir + lvl);
-                foreach (EMEditorSet mod in mapping.Any)
+                for (int i = 0; i < lvls.Count; i++)
                 {
-                    mod.ApplyToLevel(level, exclusions);
+                    Console.WriteLine("{0} - {1}", i, lvls[i]);
                 }
-                MoveLara(level);
-                writer.WriteLevelToFile(level, wdir + lvl);
-                Console.WriteLine("All AN mods applied, level saved.");
-            }
-            else if (mapping.AllWithin.Count > 0 && opt == 2)
-            {
-                int setIndex = mapping.AllWithin.Count == 1 ? 0 : GetInt(string.Format("Choose a set (0 - {0})", mapping.AllWithin.Count - 1));
-                List<EMEditorSet> set = mapping.AllWithin[setIndex];
 
-                if (set.Count > 1 && GetOption("Loop?"))
+                string lvl = lvls[GetInt("Choose a level")];
+                _readPath = Path.Combine(rdir, lvl);
+                _writePath = Path.Combine(wdir, lvl);
+
+                Console.WriteLine();
+                PickPositioning();
+
+                EMEditorMapping mapping = EMEditorMapping.Get(lvl);
+                
+                Console.WriteLine();
+                Console.WriteLine("0 - AllWithin");
+                Console.WriteLine("1 - OneOf");
+                Console.WriteLine("2 - Validate locations");
+
+                Console.WriteLine("Each All and Any functions will be applied by default.");
+                int opt = GetInt("Pick a variable option to run:");
+                Console.WriteLine();
+
+                if (opt == 0)
                 {
-                    for (int i = 0; i < set.Count; i++)
+                    RunAllWithin(mapping);
+                }
+                else if (opt == 1)
+                {
+                    RunOneOf(mapping);
+                }
+                else if (opt == 2)
+                {
+                    if (GetOption("Validate all levels?"))
                     {
-                        TR2Level level = reader.ReadLevel(rdir + lvl);
-                        EMEditorSet mod = set[i];
-                        mod.ApplyToLevel(level, exclusions);
-                        MoveLara(level);
-                        writer.WriteLevelToFile(level, wdir + lvl);
-                        Console.WriteLine("Mod {0} applied, level saved...press any key to continue.", i);
-                        Console.ReadLine();
+                        foreach (string l in LevelNames.AsListWithAssault)
+                        {
+                            mapping = EMEditorMapping.Get(l);
+                            Console.WriteLine("Checking " + l);
+                            TR2Level level = _reader.ReadLevel(rdir + l);
+                            ValidateLocations(level, mapping);
+                            Console.WriteLine();
+                        }
+                    }
+                    else
+                    {
+                        TR2Level level = _reader.ReadLevel(rdir + lvl);
+                        ValidateLocations(level, mapping);
+                        Console.WriteLine();
                     }
                 }
-                else
-                {
-                    TR2Level level = reader.ReadLevel(rdir + lvl);
-                    int modIndex = set.Count == 1 ? 0 : GetInt(string.Format("Choose a mod for AW {0} (0 - {1})", setIndex, set.Count - 1));
-                    EMEditorSet mod = set[modIndex];
-                    mod.ApplyToLevel(level, exclusions);
-                    MoveLara(level);
-                    writer.WriteLevelToFile(level, wdir + lvl);
-                    Console.WriteLine("Mod applied, level saved.");
-                }
-                Console.WriteLine();
-            }
-            else if (mapping.OneOf.Count > 0 && opt == 3)
-            {
-                int setIndex = mapping.OneOf.Count == 1 ? 0 : GetInt(string.Format("Choose a set (0 - {0})", mapping.OneOf.Count - 1));
-                EMEditorGroupedSet set = mapping.OneOf[setIndex];
 
-                if (set.Followers.Length > 1 && GetOption("Loop?"))
-                {
-                    for (int i = 0; i < set.Followers.Length; i++)
-                    {
-                        TR2Level level = reader.ReadLevel(rdir + lvl);
-                        EMEditorSet mod = set.Followers[i];
-                        set.ApplyToLevel(level, mod, exclusions);
-                        MoveLara(level);
-                        writer.WriteLevelToFile(level, wdir + lvl);
-                        Console.WriteLine("Mod {0} applied, level saved...press any key to continue.", i);
-                        Console.ReadLine();
-                    }
-                }
-                else
-                {
-                    int modIndex = set.Followers.Length == 1 ? 0 : GetInt(string.Format("Choose a follower mod (0 - {0})", set.Followers.Length - 1));
-                    EMEditorSet mod = set.Followers[modIndex];
-                    TR2Level level = reader.ReadLevel(rdir + lvl);
-                    set.ApplyToLevel(level, mod, exclusions);
-                    MoveLara(level);
-                    writer.WriteLevelToFile(level, wdir + lvl);
-                    Console.WriteLine("Mod applied, level saved.");
-                }
-                Console.WriteLine();
+                Breaker();
             }
-            else if (opt == 4)
+        }
+
+        static void PickPositioning()
+        {
+            Console.WriteLine("Choose Lara's position [X, Y, Z, Room] or leave blank to use the default.");
+            Console.WriteLine("Current: {0}", _laraPos);
+            string newPos = Console.ReadLine();
+            if (!string.IsNullOrEmpty(newPos))
             {
-                foreach (string l in LevelNames.AsListWithAssault)
-                {
-                    mapping = EMEditorMapping.Get(l);
-                    Console.WriteLine("Checking " + l);
-                    TR2Level level = reader.ReadLevel(rdir + l);
-                    ValidateLocations(level, mapping);
-                }
-                Console.Write("Done");
+                _laraPos = newPos;
             }
 
-            Console.Read();
+            Console.WriteLine("Move items to Lara [3, 5, 9...etc] or leave blank to use the default.");
+            Console.WriteLine("Current: {0}", _laraItems);
+            string newItems = Console.ReadLine();
+            if (!string.IsNullOrEmpty(newItems))
+            {
+                _laraItems = newItems;
+            }
+        }
+
+        static TR2Level DefaultLoadLevel(EMEditorMapping mapping)
+        {
+            // Read in the level and apply the All and Any mods by default.
+            // Also move Lara and any items we need.
+            TR2Level level = _reader.ReadLevel(_readPath);
+            MoveLara(level);
+
+            mapping.All.ApplyToLevel(level, _exclusions);
+
+            foreach (EMEditorSet mod in mapping.Any)
+            {
+                mod.ApplyToLevel(level, _exclusions);
+            }
+
+            return level;
+        }
+
+        static void WriteLevel(TR2Level level)
+        {
+            _writer.WriteLevelToFile(level, _writePath);
+        }
+
+        static void Breaker()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
         }
 
         static void MoveLara(TR2Level level)
         {
-            TR2Entity lara = level.Entities.ToList().Find(e => e.TypeID == 0);
-            string[] pos = "48650, 1530, 71161".Split(',');
-            lara.X = int.Parse(pos[0].Trim());
-            lara.Y = int.Parse(pos[1].Trim());
-            lara.Z = int.Parse(pos[2].Trim());
-            lara.Room = 26;
+            int lara = level.Entities.ToList().FindIndex(e => e.TypeID == 0);
+            string[] pos = _laraPos.Split(',');
 
-            //int[] arr = new int[] { 149, 84, 96 };
-            //foreach (int ent in arr)
-            //{
-            //    level.Entities[ent].X = lara.X;
-            //    level.Entities[ent].Y = lara.Y;
-            //    level.Entities[ent].Z = lara.Z;
-            //    level.Entities[ent].Room = lara.Room;
-            //}
+            int x = int.Parse(pos[0].Trim());
+            int y = int.Parse(pos[1].Trim());
+            int z = int.Parse(pos[2].Trim());
+            short room = short.Parse(pos[3].Trim());
+
+            List<int> ents = new List<int>() { lara };
+
+            string[] items = _laraItems.Split(',');
+            foreach (string item in items)
+            {
+                if (int.TryParse(item, out int i))
+                {
+                    ents.Add(i);
+                }
+            }
+
+            foreach (int ent in ents)
+            {
+                level.Entities[ent].X = x;
+                level.Entities[ent].Y = y;
+                level.Entities[ent].Z = z;
+                level.Entities[ent].Room = room;
+            }
         }
 
         static bool GetOption(string msg)
         {
-            Console.WriteLine(msg + " [Y/N]");
+            Console.Write(msg + " [Y/N]: ");
             string s = Console.ReadLine();
             return s.Trim().ToLower().StartsWith("y");
         }
 
         static int GetInt(string msg)
         {
-            Console.WriteLine(msg);
+            Console.Write(msg + ": ");
             string s = Console.ReadLine();
             return int.TryParse(s, out int i) ? i : GetInt(msg);
+        }
+
+        static void RunAllWithin(EMEditorMapping mapping)
+        {
+            if (mapping.AllWithin.Count > 0)
+            {
+                int setIndex = mapping.AllWithin.Count == 1 ? 0 : GetInt(string.Format("Choose a set (0 - {0})", mapping.AllWithin.Count - 1));
+                List<EMEditorSet> set = mapping.AllWithin[setIndex];
+
+                int modIndex = set.Count == 1 ? 0 : GetInt(string.Format("Choose a mod for AW {0} (0 - {1}). Enter -1 to loop", setIndex, set.Count - 1));
+                if (modIndex == -1)
+                {
+                    for (int i = 0; i < set.Count; i++)
+                    {
+                        TR2Level level = DefaultLoadLevel(mapping);
+
+                        EMEditorSet mod = set[i];
+                        mod.ApplyToLevel(level, _exclusions);
+
+                        WriteLevel(level);
+                        Console.WriteLine("Mod {0} applied, level saved...press return to continue.", i);
+                        Console.ReadLine();
+                    }
+                    Console.WriteLine("All mods executed.");
+                }
+                else
+                {
+                    TR2Level level = DefaultLoadLevel(mapping);
+
+                    EMEditorSet mod = set[modIndex];
+                    mod.ApplyToLevel(level, _exclusions);
+
+                    WriteLevel(level);
+                    Console.WriteLine("Mod applied, level saved.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("AllWithin group is empty.");
+            }
+        }
+
+        static void RunOneOf(EMEditorMapping mapping)
+        {
+            if (mapping.OneOf.Count > 0)
+            {
+                int setIndex = mapping.OneOf.Count == 1 ? 0 : GetInt(string.Format("Choose a set (0 - {0})", mapping.OneOf.Count - 1));
+                EMEditorGroupedSet set = mapping.OneOf[setIndex];
+
+                int modIndex = set.Followers.Length == 1 ? 0 : GetInt(string.Format("Choose a follower mod (0 - {0}). Enter -1 to loop", set.Followers.Length - 1));
+
+                if (modIndex == -1)
+                {
+                    Console.WriteLine();
+                    for (int i = 0; i < set.Followers.Length; i++)
+                    {
+                        TR2Level level = DefaultLoadLevel(mapping);
+
+                        EMEditorSet mod = set.Followers[i];
+                        set.ApplyToLevel(level, mod, _exclusions);
+
+                        WriteLevel(level);
+                        Console.WriteLine("Mod {0} applied, level saved...press enter to continue.", i);
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    TR2Level level = DefaultLoadLevel(mapping);
+
+                    EMEditorSet mod = set.Followers[modIndex];
+                    set.ApplyToLevel(level, mod, _exclusions);
+
+                    WriteLevel(level);
+                    Console.WriteLine("Mod applied, level saved.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("OneOf group is empty.");
+            }
         }
 
         static void ValidateLocations(TR2Level level, EMEditorMapping mapping)
@@ -225,17 +328,38 @@ namespace TREnvironmentEditorTests
         {
             if (location == null)
             {
-                Console.WriteLine("Warning - null location found.");
+                if (!(f is EMSwapSlotFunction))
+                {
+                    Console.WriteLine("Warning - null location found.");
+                }
             }
             else if (!TestLocation(level, location))
             {
                 Console.WriteLine("Location is outwith room bounds - " + ToString(location));
+            }
+            else if (!TestAngle(location.Angle))
+            {
+                Console.WriteLine("Location's angle % 16384 != 0 : " + location.Angle);
+            }
+            else if (f is EMMoveSlotFunction)
+            {
+                if (!TestFlip(level, location))
+                {
+                    // We don't want to move slots, switches etc in flip map rooms
+                    Console.WriteLine("Room has flip map - check there are no flip map triggers - " + ToString(location));
+                }
+                if (!TestSlope(level, location))
+                {
+                    // Slots, levers etc don't work on sloped floors unless underwater
+                    Console.WriteLine("Slot is positioned on a slope - " + ToString(location));
+                }
             }
         }
 
         static bool TestLocation(TR2Level level, EMLocation location)
         {
             TR2Room room = level.Rooms[location.Room];
+
             int minX = room.Info.X;
             int maxX = room.Info.X + 1024 * room.NumXSectors;
             int minZ = room.Info.Z;
@@ -246,6 +370,49 @@ namespace TREnvironmentEditorTests
             return minX <= location.X && maxX >= location.X &&
                 minY <= location.Y && maxY >= location.Y &&
                 minZ <= location.Z && maxZ >= location.Z;
+        }
+
+        static bool TestFlip(TR2Level level, EMLocation location)
+        {
+            TR2Room room = level.Rooms[location.Room];
+            if (room.AlternateRoom != -1)
+            {
+                FDControl control = new FDControl();
+                control.ParseFromLevel(level);
+
+                TRRoomSector flipSector = FDUtilities.GetRoomSector(location.X, location.Y, location.Z, room.AlternateRoom, level, control);
+                if (flipSector.FDIndex != 0)
+                {
+                    List<FDEntry> triggers = control.Entries[flipSector.FDIndex].FindAll(e => e is FDTriggerEntry);
+                    return triggers.Count == 0;
+                }
+            }
+            return true;
+        }
+
+        static bool TestSlope(TR2Level level, EMLocation location)
+        {
+            if (level.Rooms[location.Room].ContainsWater)
+            {
+                return true;
+            }
+
+            FDControl control = new FDControl();
+            control.ParseFromLevel(level);
+
+            TRRoomSector sector = FDUtilities.GetRoomSector(location.X, location.Y, location.Z, location.Room, level, control);
+            if (sector.FDIndex != 0)
+            {
+                List<FDEntry> slants = control.Entries[sector.FDIndex].FindAll(e => e is FDSlantEntry s && s.Type == FDSlantEntryType.FloorSlant);
+                return slants.Count == 0;
+            }
+
+            return true;
+        }
+
+        static bool TestAngle(short angle)
+        {
+            return angle % 16384 == 0;
         }
 
         static string ToString(EMLocation location)
